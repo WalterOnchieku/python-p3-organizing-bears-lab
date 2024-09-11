c072ab4984e3594253160e4ff0029a7f87b4511f
root <root@DESKTOP-RHA5CJR.localdomain>
2024-09-11 16:30:30 +0300

    organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Mr. Chocolate', 3, 'M', 'brown', 'mild', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Rowdy', 5, 'M', 'yellow', 'happy', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Tabitha', 4, 'F', 'blue', 'jumpy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Sergeant Brown', 7, 'M', 'white', 'gentle', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Melissa', 1, 'F', 'pink', 'mild', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Grinch', 3, 'M', 'green', 'angry', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Wendy', 4, 'F', 'orange', 'nice', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL, NULL, NULL, NULL, NULL, NULL);
